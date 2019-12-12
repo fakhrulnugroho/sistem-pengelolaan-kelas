111c0fb35ea2cee05abed7ff9fa057fb0845a898
Fakhrul <fakhrl.fanani.nugroho@gmail.com>
2019-12-12 21:51:55 +0700

    mengubah base url

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php 
 
 function base_url($path = null){
-	$base_url = "http://localhost/sistem_pengelolaan_kelas/";
+	$base_url = "http://localhost/sistem-pengelolaan-kelas/";
 	if($path == null){
 		return $base_url;
 	} else {
